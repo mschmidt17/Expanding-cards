617ce506980dd755ea6f2dd726aed8c96427f973
Matias Schmidt <matias.schmidt@outlook.es>
2022-08-09 11:11:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
+
 function App() {
+  const removeActive = (e) => {
+    var element = document.querySelectorAll("div")
+    element.forEach(e => e.classList.remove("active"));
+  }
+
+  const handleClick = (e) => {
+    removeActive()
+    e.currentTarget.classList.add('active')
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="panel active" id="panel" onClick={handleClick}>
+        <h3 className="text">Explore The Islands</h3>
+      </div>
+      <div className="panel1"  id="panel" onClick={handleClick}>
+        <h3 className="text"> Waterfall </h3>
+      </div>
+      <div className="panel2"  id="panel" onClick={handleClick}>
+        <h3 className="text">Sunny Beach</h3>
+      </div>
+      <div className="panel3 "  id="panel" onClick={handleClick}>
+        <h3 className="text">Sunrise</h3>
+      </div>
+      <div className="panel4"  id="panel" onClick={handleClick}>
+        <h3 className="text">Sunset</h3>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
